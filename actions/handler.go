38791ff3c74eab6d67f3790fd1e286900ce03525
jtaylor <jonrtaylor12@gmail.com>
2023-06-02 11:57:57 -0600

    gpt docs

--- actions/handler.go
+++ actions/handler.go
@@ -7,12 +7,19 @@ import (
 	"github.com/jt0610/scaf/yaml"
 )
 
+// Handler is a type that combines context.Context with Services related to system.System and system.Module.
+// The services are responsible for handling operations related to instances of system.System and system.Module.
 type Handler struct {
-	Ctx           context.Context
+	// Ctx is the context in which the handler operates. This can be used to handle cancellations, timeouts, and passing request-scoped values.
+	Ctx context.Context
+	// SystemService is a service.Service specialized to work with system.System.
 	SystemService service.Service[*system.System]
-	ModService    service.Service[*system.Module]
+	// ModService is a service.Service specialized to work with system.Module.
+	ModService service.Service[*system.Module]
 }
 
+// YamlHandler returns a new instance of Handler with its context set to the provided context and its services initialized to instances of yaml.YAMLService for system.System and system.Module.
+// This can be used to handle operations related to system.System and system.Module in a context where YAML is used as the data interchange format.
 func YamlHandler(ctx context.Context) *Handler {
 	return &Handler{
 		Ctx:           ctx,
--- actions/system.go
+++ actions/system.go
@@ -7,6 +7,9 @@ import (
 	"io"
 )
 
+// CreateSystem is a method of Handler that facilitates the creation of a new system.
+// It uses a wizard to interactively gather the necessary information from the user, populates the metadata of the system,
+// and then uses the SystemService associated with the Handler to write the new system data to the provided io.Writer.
 func (h *Handler) CreateSystem(ctx context.Context, writer io.Writer) error {
 	w := wizard.Wizard[system.System]{}
 	sys, err := w.Run(ctx)
